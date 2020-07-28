1c22a7d4da677d2fdb62c7930ac2a88741011a78
cuongtrinh5192 <cuongtrinh5192@gmail.com>
2020-07-28 16:25:19 +0700

    up code

---
+++ T1904E/QualityTest/Controller/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using QualityTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityTest.Controller
+{
+    class StudentController : Student
+    {
+        List<Student> lsStudents = new List<Student>();
+        public void PrintListStudent(List<Student> students)
+        {
+           
+            
+                Console.WriteLine("RollNumber : {0}", RollNumber);
+                Console.WriteLine("FullName : {0}", FullName);
+                Console.WriteLine("Birthday : {0}", Birthday);
+                Console.WriteLine("Email : {0}", Email);
+                Console.WriteLine("Phone : {0}", Phone);
+                Console.WriteLine("Create at : {0}", CreatedAt);
+                Console.WriteLine("Status : {0}", Status);
+                       
+        }
+
+        public void CreateStudent()
+        {
+            Console.Write("Roll Number : ");
+            RollNumber = Console.ReadLine();
+            Console.Write("FullName : ");
+            FullName = Console.ReadLine();
+            Console.Write("Birthday : ");
+            Birthday = DateTime.Parse(Console.ReadLine());
+            Console.Write("Email : ");
+            Email = Console.ReadLine();
+            Console.Write("Phone : ");
+            Phone = Console.ReadLine();
+            Console.Write("Created at : ");
+            CreatedAt = DateTime.Parse(Console.ReadLine());
+            Console.Write("Status ACTIVE(1) / DEACTIVE (0): ");
+            Status = (StudentStatus)byte.Parse(Console.ReadLine());
+           
+        }
+    }
+}
---
+++ T1904E/QualityTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using QualityTest.Controller;
+using QualityTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityTest
+{
+    class Program : Student
+    {
+        
+        static public int GenerateMenu()
+        {
+            Console.WriteLine("----------Student Manager----------");
+            Console.WriteLine("1. Create Student");
+            Console.WriteLine("2. Display Student List");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter choice : ");
+            int choice = Convert.ToInt16(Console.ReadLine());
+            return choice;
+        }
+        static void Main(string[] args)
+        {
+            List<Student> lsStudents = new List<Student>();
+            
+            int choice = 0;
+            do
+            {
+                choice = GenerateMenu();
+                switch (choice)
+                {
+                    case 1:
+                        StudentController controller = new StudentController();
+                        controller.CreateStudent();
+                        lsStudents.Add(controller);
+                        break;
+                    case 2:
+                        foreach (StudentController sc in lsStudents)
+                        {
+                            sc.PrintListStudent(lsStudents);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 3);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
