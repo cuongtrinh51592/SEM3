0e2c2dcfba112991f9fb39436396bc5c7b80a559
cuongtrinh5192 <cuongtrinh5192@gmail.com>
2020-08-29 23:02:38 +0700

    lap11

---
+++ LAP11/LAP11/Models/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAP11.Models
+{
+    public class SoundManager
+    {
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+            allSounds.ForEach(p => sounds.Add(p));
+        }
+
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCateGory)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == soundCateGory).ToList(); ;
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+        public static void GetSoundsByName(ObservableCollection<Sound> sounds, string name)
+        {
+            var allSounds = getSounds();
+            var filteredSound = allSounds.Where(p => p.Name == name).ToList();
+            sounds.Clear();
+            filteredSound.ForEach(p => sounds.Add(p));
+
+        }
+    }
+}
