63285016ae2a364ca076cb042ddf18876344fe9e
cuongtrinh5192 <cuongtrinh5192@gmail.com>
2020-09-26 16:12:33 +0700

    Practice

---
+++ ProductManager/ProductManager/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
+using ProductManager.Services;
+
+namespace ProductManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
---
+++ ProductManager/ProductManager/Data/ProductManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductManager.Models;
+
+namespace ProductManager.Data
+{
+    public class ProductManagerContext : DbContext
+    {
+        public ProductManagerContext (DbContextOptions<ProductManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductManager.Models.Product> Product { get; set; }
+    }
+}
